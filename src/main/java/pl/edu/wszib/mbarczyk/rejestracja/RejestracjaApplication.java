386d99e94481070cc319fed3ba51d2d428ac0dd9
Marcin Barczyk <hebrayah@gmail.com>
2025-01-02 15:57:42 +0100

    initializr

---
+++ src/main/java/pl/edu/wszib/mbarczyk/rejestracja/RejestracjaApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.mbarczyk.rejestracja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RejestracjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RejestracjaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/wszib/mbarczyk/rejestracja/RejestracjaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.mbarczyk.rejestracja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RejestracjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
